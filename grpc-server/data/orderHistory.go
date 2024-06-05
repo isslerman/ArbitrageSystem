2625e7176c539a82031a2c1ad3d0ac85d6f4166d
Marcos Issler <marcos@bosshouse.com.br>
2024-06-05 14:52:45 -0300

    added comments

--- grpc-server/data/orderHistory.go
+++ grpc-server/data/orderHistory.go
@@ -3,9 +3,11 @@ package data
 type AskOrder OrderHistory
 type BidOrder OrderHistory
 
+// OrderHistory contains information about an order that was offerred as the best bid or ask on a particular exchange.
+// It is used to manage all the best orders received from each exchange and to calculate where is the best price for buy or sell.
 type OrderHistory struct {
-	ExcID    string
-	Price    float64
-	PriceVET float64
-	Volume   float64
+	ExcID    string  // Exchange ID. Ex. BINA
+	Price    float64 // Price of the order
+	PriceVET float64 // Price of the order with fees included. VET is calculated.
+	Volume   float64 // Volume of the order
 }
